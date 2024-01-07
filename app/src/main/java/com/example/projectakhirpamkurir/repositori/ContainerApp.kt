34bd26dce09803d12430cb327c02bb4421f442a6
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2024-01-07 15:05:33 +0700

    membuat class ContainerDataApp

--- app/src/main/java/com/example/projectakhirpamkurir/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/projectakhirpamkurir/repositori/ContainerApp.kt
@@ -1,6 +1,15 @@
 package com.example.projectakhirpamkurir.repositori
 
+import android.content.Context
+import com.example.projectakhirpamkurir.data.DatabaseKurir
+
 interface ContainerApp {
     val repositoriKurir : RepositoriKurir
+}
+
+class ContainerDataApp(private val context: Context): ContainerApp {
+    override val repositoriKurir: RepositoriKurir by lazy {
+        OfflineRepositoriKurir(DatabaseKurir.getDatabase(context).kurirDao())
+    }
 }
 
