8da0185bda84d690461c8d33f9bfa7f8649a4b68
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2024-01-07 19:48:12 +0700

    import state di EditViewModel

--- app/src/main/java/com/example/projectakhirpamkurir/model/EditViewModel.kt
+++ app/src/main/java/com/example/projectakhirpamkurir/model/EditViewModel.kt
@@ -2,6 +2,7 @@ package com.example.projectakhirpamkurir.model
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
