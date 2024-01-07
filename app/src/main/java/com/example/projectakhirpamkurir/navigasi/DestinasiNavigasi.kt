b90fb1c847ab32dea169098f017e8cb94a8d7111
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2024-01-07 21:34:32 +0700

    menambahkan DestinasiNavigasi

--- app/src/main/java/com/example/projectakhirpamkurir/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/projectakhirpamkurir/navigasi/DestinasiNavigasi.kt
@@ -1,4 +1,8 @@
 package com.example.projectakhirpamkurir.navigasi
 
 interface DestinasiNavigasi {
+    //Nama unik untuk menentukan jalur untuk composable
+    val route: String
+    //String resource id yang berisi judul yang akan ditampilkan dilayar halaman
+    val titleRes: Int
 }
