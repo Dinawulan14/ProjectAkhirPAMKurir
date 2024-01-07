12f5fe88799d2742b0828b831d67f77c85785fa2
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2024-01-07 21:06:41 +0700

    menambahkan PenyediaViewModel dan function

--- app/src/main/java/com/example/projectakhirpamkurir/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/projectakhirpamkurir/model/PenyediaViewModel.kt
@@ -1,4 +1,38 @@
 package com.example.projectakhirpamkurir.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.projectakhirpamkurir.AplikasiKurir
+
 object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiKurir().container.repositoriKurir)
+        }
+        initializer {
+            EntryViewModel(aplikasiKurir().container.repositoriKurir)
+        }
+        initializer {
+            DetailViewModel.DetailViewModel(
+                createSavedStateHandle(),
+                aplikasiKurir ().container.repositoriKurir,
+            )
+        }
+        initializer {
+            EditViewModel(
+                createSavedStateHandle(),
+                aplikasiKurir().container.repositoriKurir,
+            )
+        }
+    }
 }
+
+/**
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari [AplikasiKurir].
+ */
+fun CreationExtras.aplikasiKurir(): AplikasiKurir =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiKurir)
