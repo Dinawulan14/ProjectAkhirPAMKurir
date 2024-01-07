65d7d5d1122f0f162b44e715cb5f0b967d063c7e
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2024-01-07 14:24:49 +0700

    Membuat Dao pada database

---
+++ app/src/main/java/com/example/projectakhirpamkurir/data/KurirDao.kt
@@ -0,0 +1,25 @@
+package com.example.projectakhirpamkurir.data
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+interface KurirDao{
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(kurir: Kurir)
+
+    @Update
+    suspend fun update(kurir: Kurir)
+
+    @Delete
+    suspend fun  delete(kurir: Kurir)
+
+    @Query("SELECT * from tblKurir WHERE id = :id")
+    fun getKurir(id: Int): Flow<Kurir>
+
+    @Query("SELECT * from tblKurir ORDER BY alamat ASC")
+    fun getAllKurir(): Flow<List<Kurir>>
+}
