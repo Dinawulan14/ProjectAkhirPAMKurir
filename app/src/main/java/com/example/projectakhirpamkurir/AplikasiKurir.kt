5e03ce4bb8e359342b0e97c03dc267941ad196b7
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2024-01-07 20:58:11 +0700

    menambahkan class AplikasiKurir

--- app/src/main/java/com/example/projectakhirpamkurir/AplikasiKurir.kt
+++ app/src/main/java/com/example/projectakhirpamkurir/AplikasiKurir.kt
@@ -1,4 +1,15 @@
 package com.example.projectakhirpamkurir
 
-class AplikasiKurir {
+import android.app.Application
+import com.example.projectakhirpamkurir.repositori.ContainerApp
+import com.example.projectakhirpamkurir.repositori.ContainerDataApp
+
+class AplikasiKurir : Application() {
+    //AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
