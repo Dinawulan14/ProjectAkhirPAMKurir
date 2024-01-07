0edcd03503e00298fb1ef6236afe1eb1544901df
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2024-01-07 19:18:16 +0700

    menambahkan class DetailViewModel

--- app/src/main/java/com/example/projectakhirpamkurir/model/DetailViewModel.kt
+++ app/src/main/java/com/example/projectakhirpamkurir/model/DetailViewModel.kt
@@ -1,4 +1,42 @@
 package com.example.projectakhirpamkurir.model
 
-class DetailViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.projectakhirpamkurir.repositori.RepositoriKurir
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class DetailViewModel {class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriKurir: RepositoriKurir
+) : ViewModel() {
+
+    private val kurirId: Int = checkNotNull(savedStateHandle[DetailsDestination.kurirIdArg])
+    val uiState: StateFlow<ItemDetailUiState> =
+        repositoriKurir.getKurirStream(kurirId)
+            .filterNotNull()
+            .map {
+                ItemDetailUiState(detailKurir = it.toDetailKurir())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = ItemDetailUiState()
+            )
+    suspend fun deleteItem() {
+        repositoriKurir.deleteKurir(uiState.value.detailKurir.toKurir())
+    }
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
+
+}
+data class ItemDetailUiState(
+    val outOfStock: Boolean = true,
+    val detailKurir: DetailKurir = DetailKurir()
+)
