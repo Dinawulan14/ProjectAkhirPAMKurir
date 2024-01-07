cb0c805b80787b109add968be8b591003acc6d51
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2024-01-07 20:01:55 +0700

    menambahkan class HomeViewModel

--- app/src/main/java/com/example/projectakhirpamkurir/model/HomeViewModel.kt
+++ app/src/main/java/com/example/projectakhirpamkurir/model/HomeViewModel.kt
@@ -1,4 +1,29 @@
 package com.example.projectakhirpamkurir.model
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.projectakhirpamkurir.data.Kurir
+import com.example.projectakhirpamkurir.repositori.RepositoriKurir
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel (private val repositoriKurir: RepositoriKurir): ViewModel(){
+    companion object {
+        private const val TIMEOUT_MILIIS = 5_000L
+    }
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriKurir.getAllKurirStream().filterNotNull()
+        .map {HomeUiState(listKurir = it.toList()) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILIIS),
+            initialValue = HomeUiState()
+        )
+
+    data class HomeUiState(
+        val listKurir: List<Kurir> = listOf()
+    )
 }
