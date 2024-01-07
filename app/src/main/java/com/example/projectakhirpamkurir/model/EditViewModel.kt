73759eb7c0708266e13e7a61ee7adfb340684c52
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2024-01-07 19:34:56 +0700

    menambahkan file di class EditlViewModel

--- app/src/main/java/com/example/projectakhirpamkurir/model/EditViewModel.kt
+++ app/src/main/java/com/example/projectakhirpamkurir/model/EditViewModel.kt
@@ -1,5 +1,48 @@
 package com.example.projectakhirpamkurir.model
 
-class EditViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.projectakhirpamkurir.repositori.RepositoriKurir
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class EditViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoriKurir: RepositoriKurir
+) : ViewModel() {
+
+    var kurirUiState by mutableStateOf(UIStateKurir())
+        private set
+        private val itemId: Int = checkNotNull(savedStateHandle[ItemEditDestination.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            kurirUiState = repositoriKurir.getKurirStream(itemId)
+                .filterNotNull()
+                .first()
+                .toUiStateKurir(true)
+        }
+    }
+    suspend fun updateKurir() {
+        if (validasiInput(kurirUiState.detailKurir)) {
+            repositoriKurir.updateKurir(kurirUiState.detailKurir.toKurir())
+        } else {
+            println("Data tidak valid")
+        }
+    }
+    fun updateUiState(detailKurir: DetailKurir) {
+        kurirUiState =
+            UIStateKurir(detailKurir = detailKurir, isEntryValid = validasiInput(detailKurir))
+    }
+
+    private fun validasiInput(uiState: DetailKurir = kurirUiState.detailKurir ): Boolean {
+        return with(uiState){
+            alamat.isNotBlank() && telepon.isNotBlank() && beratbarang.isNotBlank() && statuspengiriman.isNotBlank() && namakurir.isNotBlank()
+        }
+    }
 
 }
