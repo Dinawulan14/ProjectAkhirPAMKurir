b9083bf5ef61775f45ec75859b03161d3afa11e0
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2024-01-07 14:10:38 +0700

    Membuat tabel pada database

---
+++ app/src/main/java/com/example/projectakhirpamkurir/data/Kurir.kt
@@ -0,0 +1,16 @@
+package com.example.projectakhirpamkurir.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblKurir")
+data class Kurir(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val alamat: String,
+    val telepon: String,
+    val namacustomer: String,
+    val beratbarang: String,
+    val statuspengiriman: String,
+    val namakurir: String
+)
