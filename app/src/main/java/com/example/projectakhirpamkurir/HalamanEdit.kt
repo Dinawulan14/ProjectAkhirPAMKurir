9a1893ffb1ea27a8bdd63d4814e794b8a7a93aaf
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2024-01-08 11:07:36 +0700

    menambahkan HalamanEdit

--- app/src/main/java/com/example/projectakhirpamkurir/HalamanEdit.kt
+++ app/src/main/java/com/example/projectakhirpamkurir/HalamanEdit.kt
@@ -1,2 +1,55 @@
 package com.example.projectakhirpamkurir
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.projectakhirpamkurir.model.EditViewModel
+import com.example.projectakhirpamkurir.model.PenyediaViewModel
+import com.example.projectakhirpamkurir.navigasi.DestinasiNavigasi
+import kotlinx.coroutines.launch
+
+object ItemEditDestination : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_data
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemEditScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            KurirTopAppBar(
+                title = stringResource(ItemEditDestination.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+
+    ) { innerPadding ->
+        EntryKurirBody(
+            uiStateKurir = viewModel.kurirUiState,
+            onKurirValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateKurir()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
