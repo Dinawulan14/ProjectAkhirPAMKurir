54876ff594fd6b016a21f6f2ab56442fd4afd149
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2024-01-08 16:02:34 +0700

    membuat Function KurirApp dan KurirTopAppBar

--- app/src/main/java/com/example/projectakhirpamkurir/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/projectakhirpamkurir/navigasi/PengelolaHalaman.kt
@@ -1,2 +1,23 @@
 package com.example.projectakhirpamkurir.navigasi
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun KurirApp(navController: NavHostController = rememberNavController()){
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun KurirTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
