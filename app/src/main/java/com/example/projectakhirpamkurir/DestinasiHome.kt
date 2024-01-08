0f4f07486b2965a6d187449bf15018d6513b7039
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2024-01-08 12:50:40 +0700

    menambahkan function DataKurir pada HalamanHome

--- app/src/main/java/com/example/projectakhirpamkurir/DestinasiHome.kt
+++ app/src/main/java/com/example/projectakhirpamkurir/DestinasiHome.kt
@@ -1,12 +1,20 @@
 package com.example.projectakhirpamkurir
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -23,6 +31,7 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.projectakhirpamkurir.data.Kurir
 import com.example.projectakhirpamkurir.model.HomeViewModel
@@ -121,3 +130,52 @@ fun ListKurir(
         }
     }
 }
+
+@Composable
+fun DataKurir(
+    kurir: Kurir,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = kurir.namakurir,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null,
+                )
+                Text(
+                    text = kurir.telepon,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Spacer(Modifier.weight(1f))
+                Text(
+                    text = kurir.beratbarang,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                Text(
+                    text = kurir.statuspengiriman,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+            }
+            Text(
+                text = kurir.alamat,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
+
