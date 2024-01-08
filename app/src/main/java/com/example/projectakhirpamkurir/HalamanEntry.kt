aeacd5de09624a022bb69afc9be01ee958403831
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2024-01-08 11:15:00 +0700

    menambahkan function pada HalamanEntry

--- app/src/main/java/com/example/projectakhirpamkurir/HalamanEntry.kt
+++ app/src/main/java/com/example/projectakhirpamkurir/HalamanEntry.kt
@@ -1,2 +1,96 @@
 package com.example.projectakhirpamkurir
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.projectakhirpamkurir.model.DetailKurir
+import com.example.projectakhirpamkurir.model.EntryViewModel
+import com.example.projectakhirpamkurir.model.PenyediaViewModel
+import com.example.projectakhirpamkurir.model.UIStateKurir
+import com.example.projectakhirpamkurir.navigasi.DestinasiNavigasi
+import kotlinx.coroutines.launch
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_kurir
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryKurirScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            KurirTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ){innerPadding ->
+        EntryKurirBody(
+            uiStateKurir = viewModel.uiStateKurir,
+            onKurirValueChange = viewModel::updateUIState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.saveKurir()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+
+    }
+}
+
+@Composable
+fun EntryKurirBody(
+    uiStateKurir: UIStateKurir,
+    onKurirValueChange: (DetailKurir) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ){
+        FormInputKurir(
+            detailKurir = uiStateKurir.detailKurir,
+            onValueChange = onKurirValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            enabled = uiStateKurir.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.btn_submit))
+        }
+    }
+}
