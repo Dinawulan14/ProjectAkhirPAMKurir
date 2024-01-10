661fccd651b96e7640f0cf4e16c1952b8cae3dde
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2024-01-10 12:18:57 +0700

    membuat halaman utama

---
+++ app/src/main/java/com/example/projectakhirpamkurir/ui/theme/halaman/HalamanUtama.kt
@@ -0,0 +1,9 @@
+package com.example.projectakhirpamkurir.ui.theme.halaman
+
+import com.example.projectakhirpamkurir.R
+import com.example.projectakhirpamkurir.navigasi.DestinasiNavigasi
+
+object DestinasiUtama: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
