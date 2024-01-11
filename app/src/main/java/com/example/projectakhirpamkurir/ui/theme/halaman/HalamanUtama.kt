c324cf69cac8e7d9dc1cb2f52660e5de03947cc5
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2024-01-11 19:38:33 +0700

    menambahkan tampilan login pada halamanutama

--- app/src/main/java/com/example/projectakhirpamkurir/ui/theme/halaman/HalamanUtama.kt
+++ app/src/main/java/com/example/projectakhirpamkurir/ui/theme/halaman/HalamanUtama.kt
@@ -13,6 +13,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -88,6 +92,14 @@ fun HalamanUtama(
             horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
             verticalAlignment = Alignment.Bottom
         ) {
+            var emailTxt by rememberSaveable {
+                mutableStateOf("")
+            }
+            var passwordTxt by rememberSaveable {
+                mutableStateOf("")
+            }
+            var ListDataTxt: MutableList<String> = mutableListOf(emailTxt, passwordTxt)
+
             Button(
                 modifier = Modifier.weight(1f),
                 onClick = onNextButtonClicked,
